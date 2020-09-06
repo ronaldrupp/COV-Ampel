34398911e0a39d166ccaceff64161da06dc3342c
Ronald Rupp <ronrupp@Ronalds-MacBook-Pro.local>
2020-09-06 11:43:07 +0200

    changed icons and initialized FB

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { enableScreens } from "react-native-screens";
+enableScreens();
+import { NavigationContainer } from "@react-navigation/native";
+
+import Navigation from "./navigation/Navigation";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCasjAJrERU17qeJnV07ezT8-fLqAykUHQ",
+  authDomain: "cov-ampel.firebaseapp.com",
+  databaseURL: "https://cov-ampel.firebaseio.com",
+  projectId: "cov-ampel",
+  storageBucket: "cov-ampel.appspot.com",
+  messagingSenderId: "818839981481",
+  appId: "1:818839981481:web:6cbe7ed7c32615964588b8",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
